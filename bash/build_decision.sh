1d0fadff3eb593e3e633f10456f2cb160ddb68b8
PhilMFischer <philipp.fischer@dlr.de>
2020-12-09 13:35:17 +0100

    Logic fixes to build decision script
    - Fixing logical expressions

--- bash/build_decision.sh
+++ bash/build_decision.sh
@@ -50,13 +50,13 @@ makeBuildDecision() {
 		BUILD_TYPE_DECISION=development
 		
 	elif	[[ $GITHUB_REF == "refs/heads/integration" ]] || \
-			[[ $GITHUB_BASE_REF != *"refs/heads/integration" ]] && [[ $GITHUB_EVENT_NAME == "pull_request" ]] ;
+			[[ $GITHUB_BASE_REF == *"refs/heads/integration" ]] && [[ $GITHUB_EVENT_NAME == "pull_request" ]] ;
 	then
 		BUILD_TYPE_DECISION=integration
 		
-	elif	[[ $GITHUB_REF == *"refs/heads/master"* ]] || \
-			[[ $GITHUB_REF == *"refs/tags/Release"* ]] || \
-			[[ $GITHUB_BASE_REF != *"refs/heads/master"* ]] && [[ $GITHUB_EVENT_NAME == "pull_request" ]] ;
+	elif	[[ $GITHUB_REF == "refs/heads/master"* ]] || \
+			[[ $GITHUB_REF == "refs/tags/Release"* ]] || \
+			[[ $GITHUB_BASE_REF == *"refs/heads/master"* ]] && [[ $GITHUB_EVENT_NAME == "pull_request" ]] ;
 	then
 		BUILD_TYPE_DECISION=release
 		
