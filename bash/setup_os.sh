2ba502730d631d5cb4888b52cb07ab6c4011197a
PhilMFischer <philipp.fischer@dlr.de>
2020-12-10 07:54:39 +0100

    Adjusted scripts for adding additional packages to the buidl environment

--- bash/setup_os.sh
+++ bash/setup_os.sh
@@ -1,24 +1,79 @@
 #!/bin/bash
 
+#/*******************************************************************************
+# * Copyright (c) 2008-2019 German Aerospace Center (DLR), Simulation and Software Technology, Germany.
+# *
+# * This program and the accompanying materials are made available under the
+# * terms of the Eclipse Public License 2.0 which is available at
+# * http://www.eclipse.org/legal/epl-2.0.
+# *
+# * SPDX-License-Identifier: EPL-2.0
+# *******************************************************************************/
 
-# ---------- 
-# Install
-# ---------
+# --------------------------------------------------------------------------------------------
+# This script prepares the environment to install necessary packages like vtk or zmq
+# --------------------------------------------------------------------------------------------
+set +e
 
-echo "Installing tools"
+COMMAND=$0
+
+printUsage() {
+	echo "usage: ${COMMAND} -x -p <pkgfile>"
+	echo ""
+	echo "This command is calling apt to set up the OS."
+	echo ""
+	echo "Options:"
+	echo " -x, --xvfb            Option to install XVFB and Metacity. Usually needed by surefire UI tests."
+	echo " -a, --pkgs <pkgfile>  The name of of a file which contains the names for additional packages to be installed."
+	echo ""
+	echo "Copyright by DLR (German Aerospace Center)"
+}
+
+# process all command line arguments
+while [ "$1" != "" ]; do
+    case $1 in
+        -x | --xvfb )           shift
+                                INSTALL_XVFB=true
+                                ;;
+        -p | --pkgs )           shift
+                                INSTALL_PKGS=$1
+                                ;;
+        -h | --help )           printUsage
+                                exit
+                                ;;
+        * )                     printUsage
+                                exit 1
+    esac
+    shift
+done
+
+echo ""
+echo "-----------------------------------------------"
+echo "apt update"
+echo "-----------------------------------------------"
 sudo apt-get update
-sudo apt-get install openjdk-8-jdk xvfb metacity libvtk6-java libzmq5 libzmq-java libzmq-jni ant expect jq
 
-# -------------------------------------
-# openSSL installation (temp. disabled)
-# -------------------------------------
+echo ""
+echo "-----------------------------------------------"
+echo "apt install general packages"
+echo "-----------------------------------------------"
+sudo apt-get install openjdk-8-jdk ant expect jq
 
-# wget https://www.openssl.org/source/old/1.1.1/openssl-1.1.1g.tar.gz
-# tar xzvf openssl-1.1.1g.tar.gz
-# cd openssl-1.1.1g
-# ./config
-# make
-# sudo make install
-# cd ..
-  
 
+if [[ ! -z "$INSTALL_XVFB" && "$INSTALL_XVFB"==true ]]; then
+	echo ""
+	echo "-----------------------------------------------"	
+	echo "apt install metacity and xvfb"
+	echo "-----------------------------------------------"
+	echo "Detected the XVFB install option..."
+	sudo apt-get install xvfb metacity
+fi
+
+if [[ ! -z "$INSTALL_PKGS" && -f "$INSTALL_PKGS"]]; then
+	echo ""
+	echo "-----------------------------------------------"	
+	echo "apt install additional packages"
+	echo "-----------------------------------------------"
+	echo "Detected the packages file $INSTALL_PKGS..."
+	sudo apt-get install < $INSTALL_PKGS
+fi
